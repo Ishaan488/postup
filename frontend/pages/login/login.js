87e64e84b12f3f57ab2e75237913e5b8aac55cd6
Ishaan488 <ishaanbajpai732004@gmail.com>
2025-07-19 16:17:01 +0530

    Added loading status and error handling on login page

--- frontend/pages/login/login.js
+++ frontend/pages/login/login.js
@@ -2,16 +2,43 @@ document.querySelector("form").addEventListener("submit", async (e) => {
     e.preventDefault();
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
+    const loadingBarContainer = document.getElementById("loadingBarContainer");
+    const loadingBar = document.getElementById("loadingBar");
+    const loadingStatus = document.getElementById("loadingStatus");
+    const loadingCircle = document.getElementById("loadingCircle");
+    const checkStatus = document.getElementById("checkStatus");
+
+    const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+    loadingCircle.style.display = "block";
+    loadingBar.style.backgroundColor = "#c3fe95ff";
+    loadingStatus.innerText = "Fetching";
+    loadingBarContainer.style.display = "flex";
 
     const response = await fetch("http://localhost:5000/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password })
     });
-    alert("responding");
+
+    loadingStatus.innerText = "Loading";
+    loadingBarContainer.style.display = "flex";
+    await delay(1000);
+    loadingStatus.innerText = "Checking Data";
+    await delay(1000);
+
     const data = await response.json();
-    alert(data.message);
+    if (data.message != "User logged in successfully.") {
+        loadingCircle.style.display = "none";
+        loadingBar.style.backgroundColor = "#fe9795ff";
+        loadingStatus.innerText = data.message;
+    }
     if (data.message === "User logged in successfully.") {
+        loadingCircle.style.display = "none";
+        checkStatus.style.display = "block";
+        loadingBar.style.backgroundColor = "#c3fe95ff";
+        loadingStatus.innerText = "User logged in successfully";
+        await delay(1000);
         window.location.href = "http://127.0.0.1:3000/frontend/pages/home/home.html";
     }
 });
